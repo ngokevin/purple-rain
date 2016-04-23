643d79429ca3848703a8fb1a584c0ed736ff1e39
Kevin Ngo <me@ngokevin.com>
2016-04-23 03:16:44 -0700

    Updates

--- components/entity-generator.js
+++ components/entity-generator.js
@@ -4,8 +4,7 @@
 AFRAME.registerComponent('entity-generator', {
   schema: {
     mixin: {default: ''},
-    num: {default: 1000},
-    spread: {default: 20}
+    num: {default: 1000}
   },
 
   init: function () {
@@ -15,17 +14,7 @@ AFRAME.registerComponent('entity-generator', {
     for (var i = 0; i < data.num; i++) {
       var entity = document.createElement('a-entity');
       entity.setAttribute('mixin', data.mixin);
-      // Set random position with supplied spread.
-      entity.setAttribute('position', {
-        x: getSpread(data.spread),
-        y: getSpread(data.spread),
-        z: getSpread(data.spread)
-      });
       this.el.appendChild(entity);
     }
   }
 });
-
-function getSpread (spread) {
-  return Math.random() * spread - spread / 2;
-}
