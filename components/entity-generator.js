9e64a80eaa55394684da1d696bb862587f693bcd
Kevin Ngo <me@ngokevin.com>
2016-04-22 19:50:55 -0700

    Updates

---
+++ components/entity-generator.js
@@ -0,0 +1,31 @@
+/**
+ * Create ton of entities at random positions.
+ */
+AFRAME.registerComponent('entity-generator', {
+  schema: {
+    mixin: {default: ''},
+    num: {default: 1000},
+    spread: {default: 20}
+  },
+
+  init: function () {
+    var data = this.data;
+
+    // Create entities with supplied mixin.
+    for (var i = 0; i < data.num; i++) {
+      var entity = document.createElement('a-entity');
+      entity.setAttribute('mixin', data.mixin);
+      // Set random position with supplied spread.
+      entity.setAttribute('position', {
+        x: getSpread(data.spread),
+        y: getSpread(data.spread),
+        z: getSpread(data.spread)
+      });
+      this.el.appendChild(entity);
+    }
+  }
+});
+
+function getSpread (spread) {
+  return Math.random() * spread - spread / 2;
+}
